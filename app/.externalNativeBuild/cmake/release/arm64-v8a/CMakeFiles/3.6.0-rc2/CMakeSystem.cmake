61a59f03b6d25cbb97fe580bc88ca69b31734a9d
AbdulTheProgrammer <abdulahhadi@hotmail.com>
2017-06-03 23:10:22 -0400

    got shit done

--- app/.externalNativeBuild/cmake/release/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/release/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Windows-6.3.9600")
-set(CMAKE_HOST_SYSTEM_NAME "Windows")
-set(CMAKE_HOST_SYSTEM_VERSION "6.3.9600")
-set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+set(CMAKE_HOST_SYSTEM "Darwin-16.4.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "16.4.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("C:/Users/Constantin/AppData/Local/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("/Users/abdulhadimohamed/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-21")
 set(CMAKE_SYSTEM_NAME "Android")
